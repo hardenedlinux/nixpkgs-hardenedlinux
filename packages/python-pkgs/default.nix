a5ec8a957e551a5548291398075af1cbcf95c2b5
GTrunSec <gtrunsec@hardenedlinux.org>
2022-04-06 23:54:50 -0700

    python: add vault-cli

--- packages/python-pkgs/default.nix
+++ packages/python-pkgs/default.nix
@@ -5,4 +5,5 @@ final: prev: rec {
   btest = prev.python3Packages.callPackage ./btest {};
   zeekscript = prev.python3Packages.callPackage ./zeekscript {inherit py-tree-sitter;};
   py-tree-sitter = prev.python3Packages.callPackage ./py-tree-sitter {};
+  vault-cli = prev.python3Packages.callPackage ./vault-cli {};
 }
---
+++ packages/python-pkgs/vault-cli/default.nix
@@ -0,0 +1,20 @@
+{
+  stdenv,
+  lib,
+  python3Packages,
+  nixpkgs-hardenedlinux-sources,
+}:
+python3Packages.buildPythonPackage rec {
+  inherit (nixpkgs-hardenedlinux-sources.vault-cli) pname src version;
+  propagatedBuildInputs = with python3Packages; [click pyyaml jinja2 requests hvac];
+  checkInputs = with python3Packages; [pytest];
+  postPatch = ''
+    substituteInPlace setup.cfg \
+    --replace "hvac<0.10.12" "hvac"
+  '';
+  meta = with lib; {
+    description = "A configurable command-line interface tool (and python library) to interact with Hashicorp Vault";
+    homepage = "https://github.com/peopledoc/vault-cli";
+    license = licenses.asl20;
+  };
+}
