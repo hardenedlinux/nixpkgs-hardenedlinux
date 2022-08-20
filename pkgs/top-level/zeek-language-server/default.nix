2a284cf69d98bd01234aa02271358660b8829823
GTrunSec <gtrunsec@hardenedlinux.org>
2022-08-20 14:29:31 -0700

    update zeek-language-server

--- pkgs/top-level/zeek-language-server/default.nix
+++ pkgs/top-level/zeek-language-server/default.nix
@@ -10,5 +10,5 @@ in
     inherit (nixpkgs-hardenedlinux-sources.zeek-language-server) src pname version;
     nativeBuildInputs = [tree-sitter nodejs];
     doCheck = false;
-    cargoSha256 = "sha256-0F32NlqItkkVq+Madorxyb30RQPOS/+xX6PAIcMh+tc=";
+    cargoSha256 = "sha256-mkfB+Yz3D4XtaYkV59yBSumDkMld58RLqjPz7a2A2ls=";
   }
