89191a167803000446db95f14e93efad71ea3869
GTrunSec <gtrunsec@hardenedlinux.org>
2022-11-19 19:04:24 -0800

    python: update overlay

--- nix/python/overlays.nix
+++ nix/python/overlays.nix
@@ -1,26 +1,36 @@
 {
   inputs,
   cell,
-}: {
+}: let
+  packages = selfPythonPackages: pythonPackages: {
+    polars = selfPythonPackages.callPackage ./packages/polars {};
+    zed-python = selfPythonPackages.callPackage ./packages/zed {};
+    elastalert2 = selfPythonPackages.callPackage ./packages/elastalert2 {};
+    btest = selfPythonPackages.callPackage ./packages/btest {};
+    zeekscript = selfPythonPackages.callPackage ./packages/zeekscript {};
+    py-tree-sitter = selfPythonPackages.callPackage ./packages/py-tree-sitter {};
+    vault-cli = selfPythonPackages.callPackage ./packages/vault-cli {};
+    aim = selfPythonPackages.callPackage ./packages/aim {};
+    sh = selfPythonPackages.callPackage ./packages/sh {};
+  };
+in {
   default = final: prev: {
     nixpkgs-hardenedlinux-python-sources = prev.callPackage ./packages/_sources/generated.nix {};
+
+    python3 =
+      prev.python3.override
+      (
+        old: {
+          packageOverrides =
+            prev.lib.composeExtensions (old.packageOverrides or (_: _: {})) packages;
+        }
+      );
 
     python3Packages =
       prev.python3Packages.override
       (
         old: {
-          overrides = prev.lib.composeExtensions (old.packageOverrides or (_: _: {})) (selfPythonPackages: pythonPackages: let
-          in {
-            polars = selfPythonPackages.callPackage ./packages/polars {};
-            zed-python = prev.python3Packages.callPackage ./packages/zed {};
-            elastalert2 = prev.python3Packages.callPackage ./packages/elastalert2 {};
-            btest = prev.python3Packages.callPackage ./packages/btest {};
-            zeekscript = prev.python3Packages.callPackage ./packages/zeekscript {};
-            py-tree-sitter = prev.python3Packages.callPackage ./packages/py-tree-sitter {};
-            vault-cli = prev.python3Packages.callPackage ./packages/vault-cli {};
-            aim = prev.python3Packages.callPackage ./packages/aim {};
-            sh = prev.python3Packages.callPackage ./packages/sh {};
-          });
+          overrides = prev.lib.composeExtensions (old.packageOverrides or (_: _: {})) packages;
         }
       );
   };
