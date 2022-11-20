17e2d525f97dd9422533bf4b88e8b69db25a9d65
GTrunSec <gtrunsec@hardenedlinux.org>
2022-11-19 18:53:06 -0800

    fix python override

--- nix/python/overlays.nix
+++ nix/python/overlays.nix
@@ -5,14 +5,23 @@
   default = final: prev: {
     nixpkgs-hardenedlinux-python-sources = prev.callPackage ./packages/_sources/generated.nix {};
 
-    zed-python = prev.python3Packages.callPackage ./packages/zed {};
-    polars = prev.python3Packages.callPackage ./packages/polars {};
-    elastalert2 = prev.python3Packages.callPackage ./packages/elastalert2 {};
-    btest = prev.python3Packages.callPackage ./packages/btest {};
-    zeekscript = prev.python3Packages.callPackage ./packages/zeekscript {};
-    py-tree-sitter = prev.python3Packages.callPackage ./packages/py-tree-sitter {};
-    vault-cli = prev.python3Packages.callPackage ./packages/vault-cli {};
-    aim = prev.python3Packages.callPackage ./packages/aim {};
-    sh = prev.python3Packages.callPackage ./packages/sh {};
+    python3Packages =
+      prev.python3Packages.override
+      (
+        old: {
+          overrides = prev.lib.composeExtensions (old.packageOverrides or (_: _: {})) (selfPythonPackages: pythonPackages: let
+          in {
+            polars = selfPythonPackages.callPackage ./packages/polars {};
+            zed-python = prev.python3Packages.callPackage ./packages/zed {};
+            elastalert2 = prev.python3Packages.callPackage ./packages/elastalert2 {};
+            btest = prev.python3Packages.callPackage ./packages/btest {};
+            zeekscript = prev.python3Packages.callPackage ./packages/zeekscript {};
+            py-tree-sitter = prev.python3Packages.callPackage ./packages/py-tree-sitter {};
+            vault-cli = prev.python3Packages.callPackage ./packages/vault-cli {};
+            aim = prev.python3Packages.callPackage ./packages/aim {};
+            sh = prev.python3Packages.callPackage ./packages/sh {};
+          });
+        }
+      );
   };
 }
