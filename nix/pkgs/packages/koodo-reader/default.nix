041c8fbfd366ed55a727d5ade12d38bb196285db
GTrunSec <gtrunsec@hardenedlinux.org>
2022-12-22 20:22:56 -0800

    fix missing pname

--- nix/pkgs/packages/koodo-reader/default.nix
+++ nix/pkgs/packages/koodo-reader/default.nix
@@ -5,11 +5,11 @@
   openssl,
 }:
 buildYarnPackage {
-  inherit (nixpkgs-hardenedlinux-pkgs-sources.koodo-reader) src version;
+  inherit (nixpkgs-hardenedlinux-pkgs-sources.koodo-reader) pname src version;
   buildInputs = [openssl];
   installPhase = ''
-  cp -r build $out
-'';
+    cp -r build $out
+  '';
   NODE_OPTIONS = "--openssl-legacy-provider";
   yarnBuild = ''
     yarn install --offline --frozen-lockfile --ignore-engines --ignore-scripts --no-progress
@@ -17,4 +17,10 @@ buildYarnPackage {
     export PATH=$PWD/node_modules/.bin:$PATH
     yarn build
   '';
+  meta = with lib; {
+    description = "A modern ebook manager and reader with sync and backup capacities for Windows, macOS, Linux and Web";
+    homepage = "https://github.com/troyeguo/koodo-reader";
+    license = licenses.agpl3;
+    platforms = platforms.linux;
+  };
 }
